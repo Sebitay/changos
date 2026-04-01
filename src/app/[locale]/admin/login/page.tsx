7ec61463ec7446587f52184e35682f7a3b222fb4
Sebastian Valenzuela <sebastianvlop@gmail.com>
2026-04-01 18:35:59 -0300

    improved login form

--- src/app/[locale]/admin/login/page.tsx
+++ src/app/[locale]/admin/login/page.tsx
@@ -4,9 +4,17 @@ import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { useLocale, useTranslations } from "next-intl";
 import { Input } from "@/components/ui/input";
-import { Card } from "@/components/ui/card";
-import { H1 } from "@/components/ui/typography";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { H2 } from "@/components/ui/typography";
 import { Button } from "@/components/ui/button";
+import { Field, FieldLabel, FieldGroup } from "@/components/ui/field";
 
 export default function AdminLoginPage() {
   const [email, setEmail] = useState("");
@@ -18,8 +26,9 @@ export default function AdminLoginPage() {
   const locale = useLocale();
   const t = useTranslations("LoginPage");
 
-  async function handleLogin() {
-    if (!email || !password || isLoading) return;
+  async function handleLogin(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("a");
 
     setError("");
     setIsLoading(true);
@@ -33,9 +42,11 @@ export default function AdminLoginPage() {
     setIsLoading(false);
 
     if (result?.error) {
+      console.log("error");
       setError(t("errorMessage"));
       return;
     }
+    console.log("success");
 
     router.push(`/${locale}/admin`);
     router.refresh();
@@ -43,25 +54,52 @@ export default function AdminLoginPage() {
 
   return (
     <div className="flex justify-center items-center h-full">
-      <Card className="p-4 w-100 mx-auto mt-20">
-        <H1>{t("title")}</H1>
-        <Input
-          placeholder={t("email")}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder={t("password")}
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error ? <p className="text-sm text-red-500">{error}</p> : null}
-        <Button onClick={handleLogin} disabled={isLoading}>
-          {t("loginButton")}
-        </Button>
+      <Card className="w-full sm:max-w-md">
+        <CardHeader>
+          <H2>{t("title")}</H2>
+          <CardDescription className="text-red-500">
+            {error ? `${error}` : ""}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form id="login-form" onSubmit={handleLogin} className="flex">
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="login-email">{t("email")}</FieldLabel>
+                <Input
+                  id="login-email"
+                  placeholder={t("emailPlaceholder")}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Field>
+
+              <Field>
+                <FieldLabel htmlFor="login-password">
+                  {t("password")}
+                </FieldLabel>
+                <Input
+                  id="login-password"
+                  placeholder={t("passwordPlaceholder")}
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button
+            type="submit"
+            form="login-form"
+            disabled={isLoading}
+            className="w-full"
+          >
+            {t("loginButton")}
+          </Button>
+        </CardFooter>
       </Card>
-      ß
     </div>
   );
 }
--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -23,7 +23,7 @@ export function H2({
 }) {
   return (
     <h2
-      className={`scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 ${className || ""}`}
+      className={`scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 ${className || ""}`}
     >
       {children}
     </h2>
